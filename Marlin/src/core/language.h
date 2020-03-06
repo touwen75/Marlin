johnathantran/Marlin-Food-3D-Printer-Reconfig

Added experimental support for up to 6 non-extruder axes.

Marlin/src/core/language.h
@@ -191,6 +191,12 @@
 #define MSG_Z2_MAX                          "z2_max"
 #define MSG_Z3_MIN                          "z3_min"
 #define MSG_Z3_MAX                          "z3_max"
+#define MSG_I_MIN                           "i_min"
+#define MSG_I_MAX                           "i_max"
+#define MSG_J_MIN                           "j_min"
+#define MSG_J_MAX                           "j_max"
+#define MSG_K_MIN                           "k_min"
+#define MSG_K_MAX                           "k_max"
 #define MSG_Z_PROBE                         "z_probe"
 #define MSG_FILAMENT_RUNOUT_SENSOR          "filament"
 #define MSG_PROBE_Z_OFFSET                  "Probe Z Offset"
@@ -317,6 +323,10 @@
 #define MSG_Y "Y"
 #define MSG_Z "Z"
 #define MSG_E "E"
+#define MSG_I "I"
+#define MSG_J "J"
+#define MSG_K "K"
+
 #if IS_KINEMATIC
   #define MSG_A "A"
   #define MSG_B "B"

Marlin/src/module/motion.h
@@ -36,7 +36,17 @@
 
 // Axis homed and known-position states
 extern uint8_t axis_homed, axis_known_position;
-constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS);
+constexpr uint8_t xyz_bits = (_BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS)
+  #if NON_E_AXES > 3
+    | _BV(I_AXIS)
+    #if NON_E_AXES > 4
+      | _BV(J_AXIS)
+      #if NON_E_AXES > 5
+        | _BV(K_AXIS)
+      #endif
+    #endif
+  #endif
+);
 FORCE_INLINE bool all_axes_homed() { return (axis_homed & xyz_bits) == xyz_bits; }
 FORCE_INLINE bool all_axes_known() { return (axis_known_position & xyz_bits) == xyz_bits; }
 FORCE_INLINE void set_all_unhomed() { axis_homed = 0; }
@@ -47,8 +57,8 @@ constexpr float slop = 0.0001;
 
 extern bool relative_mode;
 
-extern float current_position[XYZE],  // High-level current tool position
-             destination[XYZE];       // Destination for a move
+extern float current_position[NUM_AXIS],  // High-level current tool position
+             destination[NUM_AXIS];       // Destination for a move
 
 // Scratch space for a cartesian result
 extern float cartes[XYZ];
@@ -71,7 +81,7 @@ extern float cartes[XYZ];
  * Feed rates are often configured with mm/m
  * but the planner and stepper like mm/s units.
  */
-extern const float homing_feedrate_mm_s[XYZ];
+extern const float homing_feedrate_mm_s[NON_E_AXES];
 FORCE_INLINE float homing_feedrate(const AxisEnum a) { return pgm_read_float(&homing_feedrate_mm_s[a]); }
 float get_homing_bump_feedrate(const AxisEnum axis);
 
@@ -98,7 +108,7 @@ FORCE_INLINE float pgm_read_any(const float *p) { return pgm_read_float(p); }
 FORCE_INLINE signed char pgm_read_any(const signed char *p) { return pgm_read_byte(p); }
 
 #define XYZ_DEFS(type, array, CONFIG) \
-  extern const type array##_P[XYZ]; \
+  extern const type array##_P[NON_E_AXES]; \
   FORCE_INLINE type array(AxisEnum axis) { return pgm_read_any(&array##_P[axis]); } \
   typedef void __void_##CONFIG##__
 
@@ -117,13 +127,14 @@ XYZ_DEFS(signed char, home_dir, HOME_DIR);
 
 #if HAS_SOFTWARE_ENDSTOPS
   extern bool soft_endstops_enabled;
-  extern float soft_endstop_min[XYZ], soft_endstop_max[XYZ];
-  void clamp_to_software_endstops(float target[XYZ]);
+  extern float soft_endstop_min[NON_E_AXES], soft_endstop_max[NON_E_AXES];
+  void clamp_to_software_endstops(float target[NON_E_AXES]);
   void update_software_endstops(const AxisEnum axis);
 #else
   constexpr bool soft_endstops_enabled = false;
-  constexpr float soft_endstop_min[XYZ] = { X_MIN_BED, Y_MIN_BED, Z_MIN_POS },
-                  soft_endstop_max[XYZ] = { X_MAX_BED, Y_MAX_BED, Z_MAX_POS };
+  constexpr float soft_endstop_min[NON_E_AXES] = ARRAY_N(NON_E_AXES, X_MIN_BED, Y_MIN_BED, Z_MIN_POS, I_MIN_POS, J_MIN_POS, K_MIN_POS),
+                  soft_endstop_max[NON_E_AXES] = ARRAY_N(NON_E_AXES, X_MAX_BED, Y_MAX_BED, Z_MAX_POS, I_MAX_POS, J_MAX_POS, K_MAX_POS);
+
   #define clamp_to_software_endstops(x) NOOP
   #define update_software_endstops(x) NOOP
 #endif
@@ -202,7 +213,17 @@ void clean_up_after_endstop_or_probe_move();
     ) || ENABLED(NO_MOTION_BEFORE_HOMING)
 
 #if HAS_AXIS_UNHOMED_ERR
-  bool axis_unhomed_error(const bool x=true, const bool y=true, const bool z=true);
+  bool axis_unhomed_error(const bool x=true, const bool y=true, const bool z=true
+    #if NON_E_AXES > 3
+      , const bool i=true
+      #if NON_E_AXES > 4
+        , const bool j=true
+        #if NON_E_AXES > 5
+          , const bool k=true
+        #endif
+      #endif
+    #endif
+  );
 #endif
 
 #if ENABLED(NO_MOTION_BEFORE_HOMING)
@@ -222,13 +243,13 @@ void homeaxis(const AxisEnum axis);
  */
 #if HAS_HOME_OFFSET || HAS_POSITION_SHIFT
   #if HAS_HOME_OFFSET
-    extern float home_offset[XYZ];
+    extern float home_offset[NON_E_AXES];
   #endif
   #if HAS_POSITION_SHIFT
-    extern float position_shift[XYZ];
+    extern float position_shift[NON_E_AXES];
   #endif
   #if HAS_HOME_OFFSET && HAS_POSITION_SHIFT
-    extern float workspace_offset[XYZ];
+    extern float workspace_offset[NON_E_AXES];
     #define WORKSPACE_OFFSET(AXIS) workspace_offset[AXIS]
   #elif HAS_HOME_OFFSET
     #define WORKSPACE_OFFSET(AXIS) home_offset[AXIS]
@@ -248,6 +269,18 @@ void homeaxis(const AxisEnum axis);
 #define RAW_Y_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Y_AXIS)
 #define RAW_Z_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Z_AXIS)
 
+#if defined NON_E_AXES > 3
+  #define LOGICAL_I_POSITION(POS) NATIVE_TO_LOGICAL(POS, I_AXIS)
+  #define RAW_I_POSITION(POS)     LOGICAL_TO_NATIVE(POS, I_AXIS)
+  #if defined NON_E_AXES > 4
+    #define LOGICAL_J_POSITION(POS) NATIVE_TO_LOGICAL(POS, J_AXIS)
+    #define RAW_J_POSITION(POS)     LOGICAL_TO_NATIVE(POS, J_AXIS)
+    #if defined NON_E_AXES > 5
+      #define LOGICAL_K_POSITION(POS) NATIVE_TO_LOGICAL(POS, K_AXIS)
+      #define RAW_K_POSITION(POS)     LOGICAL_TO_NATIVE(POS, K_AXIS)
+    #endif
+  #endif
+#endif
 /**
  * position_is_reachable family of functions
  */

touwen75/Marlin

Marlin/src/core/language.h
@@ -155,6 +155,12 @@
 #define STR_Z3_MAX                          "z3_max"
 #define STR_Z4_MIN                          "z4_min"
 #define STR_Z4_MAX                          "z4_max"
+#define STR_I_MIN                           "i_min"
+#define STR_I_MAX                           "i_max"
+#define STR_J_MIN                           "j_min"
+#define STR_J_MAX                           "j_max"
+#define STR_K_MIN                           "k_min"
+#define STR_K_MAX                           "k_max"
 #define STR_Z_PROBE                         "z_probe"
 #define STR_FILAMENT_RUNOUT_SENSOR          "filament"
 #define STR_PROBE_OFFSET                    "Probe Offset"
@@ -280,6 +286,9 @@
 #define STR_X "X"
 #define STR_Y "Y"
 #define STR_Z "Z"
+#define STR_I "I"
+#define STR_J "J"
+#define STR_K "K"
 #define STR_E "E"
 #if IS_KINEMATIC
   #define STR_A "A"

Marlin/src/module/motion.h
@@ -36,7 +36,17 @@
 
 // Axis homed and known-position states
 extern uint8_t axis_homed, axis_known_position;
-constexpr uint8_t xyz_bits = _BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS);
+constexpr uint8_t xyz_bits = (_BV(X_AXIS) | _BV(Y_AXIS) | _BV(Z_AXIS)
+  #if NON_E_AXES > 3
+    | _BV(I_AXIS)
+    #if NON_E_AXES > 4
+      | _BV(J_AXIS)
+      #if NON_E_AXES > 5
+        | _BV(K_AXIS)
+      #endif
+    #endif
+  #endif
+);
 FORCE_INLINE bool no_axes_homed() { return !axis_homed; }
 FORCE_INLINE bool all_axes_homed() { return (axis_homed & xyz_bits) == xyz_bits; }
 FORCE_INLINE bool all_axes_known() { return (axis_known_position & xyz_bits) == xyz_bits; }
@@ -63,7 +73,7 @@ extern xyze_pos_t current_position,  // High-level current tool position
 
 // G60/G61 Position Save and Return
 #if SAVED_POSITIONS
-  extern uint8_t saved_slots[(SAVED_POSITIONS + 7) >> 3];
+  extern uint8_t saved_slots[(SAVED_POSITIONS + 7) >> 3]; // TODO: Add support for NON_E_AXES > 3. Maybe >> NON_E_AXES
   extern xyz_pos_t stored_position[SAVED_POSITIONS];
 #endif
 
@@ -92,7 +102,7 @@ extern xyz_pos_t cartes;
  * Feed rates are often configured with mm/m
  * but the planner and stepper like mm/s units.
  */
-extern const feedRate_t homing_feedrate_mm_s[XYZ];
+extern const feedRate_t homing_feedrate_mm_s[NON_E_AXES];
 FORCE_INLINE feedRate_t homing_feedrate(const AxisEnum a) { return pgm_read_float(&homing_feedrate_mm_s[a]); }
 feedRate_t get_homing_bump_feedrate(const AxisEnum axis);
 
@@ -233,8 +243,19 @@ void restore_feedrate_and_scaling();
 // Homing
 //
 
-uint8_t axes_need_homing(uint8_t axis_bits=0x07);
-bool axis_unhomed_error(uint8_t axis_bits=0x07);
+#if NON_E_AXES == 4
+  uint8_t axes_need_homing(uint8_t axis_bits=0x0F); // 0x0F = 00001111 = all four axes
+  bool axis_unhomed_error(uint8_t axis_bits=0x0F); // 0x0F = 00001111 = all four axes
+#elif NON_E_AXES == 5
+  uint8_t axes_need_homing(uint8_t axis_bits=0x1F); // 0x1F = 00011111 = all five axes
+  bool axis_unhomed_error(uint8_t axis_bits=0x1F); // 0x1F = 00011111 = all five axes
+#elif NON_E_AXES == 6
+  uint8_t axes_need_homing(uint8_t axis_bits=0x3F); // 0x3F = 00111111 = all six axes
+  bool axis_unhomed_error(uint8_t axis_bits=0x3F); // 0x3F = 00111111 = all six axes
+#else  // 0x07 = 00000111 = all three axes
+  uint8_t axes_need_homing(uint8_t axis_bits=0x07);
+  bool axis_unhomed_error(uint8_t axis_bits=0x07);
+#endif
 
 #if ENABLED(NO_MOTION_BEFORE_HOMING)
   #define MOTION_CONDITIONS (IsRunning() && !axis_unhomed_error())
@@ -291,6 +312,18 @@ void homeaxis(const AxisEnum axis);
 #define RAW_Y_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Y_AXIS)
 #define RAW_Z_POSITION(POS)     LOGICAL_TO_NATIVE(POS, Z_AXIS)
 
+#if defined NON_E_AXES > 3
+  #define LOGICAL_I_POSITION(POS) NATIVE_TO_LOGICAL(POS, I_AXIS)
+  #define RAW_I_POSITION(POS)     LOGICAL_TO_NATIVE(POS, I_AXIS)
+  #if defined NON_E_AXES > 4
+    #define LOGICAL_J_POSITION(POS) NATIVE_TO_LOGICAL(POS, J_AXIS)
+    #define RAW_J_POSITION(POS)     LOGICAL_TO_NATIVE(POS, J_AXIS)
+    #if defined NON_E_AXES > 5
+      #define LOGICAL_K_POSITION(POS) NATIVE_TO_LOGICAL(POS, K_AXIS)
+      #define RAW_K_POSITION(POS)     LOGICAL_TO_NATIVE(POS, K_AXIS)
+    #endif
+  #endif
+#endif
 /**
  * position_is_reachable family of functions
  */